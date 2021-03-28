d0857da45dc8e9d77c70e71482b799ee7d988bb8
J31NM <jeanmaffert@yahoo.fr>
2021-03-28 18:54:59 +0200

    Create html pages for app features

--- pur_beurre/products/models.py
+++ pur_beurre/products/models.py
@@ -1,6 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# class Post(models.Model):
-#     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    nutriscore = models.CharField(max_length=1)
+    url = models.URLField(null=True)
+    picture = models.URLField(null=True, max_length=500)
+    categories = models.ManyToManyField(Category, related_name='products', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.name
 
