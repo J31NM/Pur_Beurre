dc47455db6ac8ba654f8730a3cd21e9bebceb367
J31NM <jeanmaffert@yahoo.fr>
2021-04-06 10:01:13 +0200

    add display favorites

--- pur_beurre/products/views.py
+++ pur_beurre/products/views.py
@@ -1,13 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic import ListView, DetailView
-from django.http import HttpResponse
+from django.http import HttpResponse, request
 
 from .models import Category, Product, Favorite
 
 
 def index(request):
-    # raise RuntimeError('Oh lalala')
     return render(request, 'products/index.html', {})
 
 
@@ -29,15 +31,8 @@ def error_500(request, exception=None):
 
 class Products(ListView):
     model = Product
+    paginate_by = 6
     template_name = 'products/search.html'
-
-
-class Favorites(ListView):
-    model = Favorite
-    template_name = 'products/favorites.html'
-
-    # def get(self, request, **options):
-
 
 
 class Details(ListView):
@@ -50,3 +45,19 @@ class Details(ListView):
             raise ValueError('Invalid Product ID provided')
         product = self.model.objects.get(id=int(pid))
         return render(request, self.template_name, {'product': product})
+
+
+class Favorites(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
+    # redirect_field_name = 'redirect_to'
+    model = Favorite
+    template_name = 'products/favorites.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(user=self.request.user)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["object_list"] = [favorite.product for favorite in context["object_list"]]
+        return context
