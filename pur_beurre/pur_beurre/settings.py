dc6f46f36b8c427df87ae16d84c29ad951ec5d81
J31NM <jeanmaffert@yahoo.fr>
2021-07-07 20:36:32 +0200

    var

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('../secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
-POSTGRE_KEY = os.environ.get("POSTGRE_KEY")
+# with open('../secret_key.txt') as f:
+#     SECRET_KEY = f.read().strip()
+# POSTGRE_KEY = os.environ.get("POSTGRE_KEY")
+SECRET_KEY = os.environ.get("POSTGRE_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -91,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pur_beurre_db',
         'USER': 'postgres',
-        'PASSWORD': POSTGRE_KEY,
+        'PASSWORD': SECRET_KEY,
         'HOST': 'localhost',
         'PORT': '5433',
     }
