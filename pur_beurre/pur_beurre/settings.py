f03605eff5c8bf48358b38f9d0f2320b3bd20a74
J31NM <jeanmaffert@yahoo.fr>
2021-07-07 19:34:58 +0200

    set SECRET_KEY in heroku vars

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('../secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+# with open('../secret_key.txt') as f:
+#     SECRET_KEY = f.read().strip()
 POSTGRE_KEY = os.environ.get("POSTGRE_KEY")
 
 
