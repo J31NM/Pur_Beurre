0e074f177ab25417c94a357b35c382b4f626a929
J31NM <jeanmaffert@yahoo.fr>
2021-07-27 15:59:47 +0200

    change staticfiles_Dirs

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -147,7 +147,7 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 LOGIN_REDIRECT_URL = 'home'
