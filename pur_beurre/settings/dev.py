c79eadf6fd0cef68873355faaf73954d9bb704ab
J31NM <jeanmaffert@yahoo.fr>
2022-01-25 20:39:24 +0100

    modify settings to connect github_workflow to postgres

--- pur_beurre/settings/dev.py
+++ pur_beurre/settings/dev.py
@@ -16,3 +16,15 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT', 5433),
     }
 }
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
