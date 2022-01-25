890377cc3dfdee458caf82fbb65eba4c9451f740
J31NM <jeanmaffert@yahoo.fr>
2022-01-25 20:25:21 +0100

    dev settings to configure postgres

--- pur_beurre/settings/__init__.py
+++ pur_beurre/settings/__init__.py
@@ -1,3 +1,3 @@
-from pur_beurre.settings.test import *
+# from pur_beurre.settings.test import *
 # from pur_beurre.settings.production import *
-# from pur_beurre.settings.dev import *
+from pur_beurre.settings.dev import *
